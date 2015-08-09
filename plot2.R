c0919c3f5115ea95cfa626eeeb30f31bb4473a25
Leo Arias <yo@elopio.net>
2015-08-08 18:03:44 -0600

    Added the second plot.

---
+++ plot2.R
@@ -0,0 +1,13 @@
+source("common.R")
+
+MakePlot <- function(dataset, plot.file.name) {
+  Sys.setlocale(category = "LC_ALL", locale = "C")
+  png(plot.file.name)
+  with(dataset, plot(type="l", datetime, Global_active_power,
+       xlab="", ylab="Global Active Power (kilowatts)"))
+  dev.off()
+}
+
+data.file.path <- GetData()
+dataset <- LoadDataset(data.file.path)
+MakePlot(dataset, "plot2.png")
